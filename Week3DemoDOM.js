779e9c98a03c58925dc42032b514ca15fc993593
ManjulaChandirasekaran <35280800+ManjulaChandirasekaran@users.noreply.github.com>
2018-09-25 13:01:29 -0400

    Add files via upload

---
+++ Week3DemoDOM.js
@@ -0,0 +1,25 @@
+
+window.onload = function()
+{
+    //attach event listenere for click, mouseover and mouseout
+}
+
+
+
+//Remove eventlistener
+
+
+function domTest()
+{    
+    
+    //Show content of a button - Single element demo
+    
+    //Set border for all <p> - Collection demo
+
+    //Set color for all classes for <p> - Collection/Style demo
+    
+    //Create and append a child <p> to main-content
+
+    
+}
+
---
+++ Week3DemoFunctions.js
@@ -0,0 +1,13 @@
+
+//any code outside of a function will get called when the window loads
+
+//function associated with a user-driven event 
+
+//function with no arguments and no return value
+
+//function with arguments and no return value
+
+//function with argument and return value
+
+//function that takes an object instead of a simple argument
+
